1df3c317f4efe81b988c1c150fad89d8b634e650
Jack210797 <suvorov21079@gmail.com>
2024-03-17 13:10:01 +0200

    hm15

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    screens: {
+      xl: '1280px',
+      lg: '1024px',
+      sm: '640px',
+      xs: '480px',
+      xx: '420px',
+      ux: '360px',
+    },
+    extend: {
+
+    },
+
+  },
+  plugins: [],
+}
+
